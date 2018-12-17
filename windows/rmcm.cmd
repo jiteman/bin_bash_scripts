a3c00035b91c11287c06a00fcc4e563e6d4f143b
Jiteman <jiteman@gmail.com>
2018-12-17 18:56:50 +0300

    cm remove changed to replace current recursively

--- windows/rmcm.cmd
+++ windows/rmcm.cmd
@@ -1,13 +1,3 @@
 @echo off
-if exist "../../__binaries" (
-	rmdir "../../__binaries" /S /Q
-)
 
-if exist "../../__libraries" (
-	rmdir "../../__libraries" /S /Q
-)
-
-cd ..
-rmdir cm /S /Q
-mkdir cm
-cd cm
+rmdir . /S /Q
