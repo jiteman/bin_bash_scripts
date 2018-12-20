e6c631ecbd4fd55979624e92c650d41420f2643e
Jiteman <jiteman@gmail.com>
2018-12-20 15:45:25 +0000

    working directory script set

--- windows/jinit.cmd
+++ windows/jinit.cmd
@@ -1,14 +1,13 @@
 @echo off
 
+rem @echo on
+
 set JMSD_BIN_ROOT=%~dp0
 
 call %JMSD_BIN_ROOT%/.config/jvc
 call %JMSD_BIN_ROOT%/.config/jqt
 call %JMSD_BIN_ROOT%/.config/jjom
 call %JMSD_BIN_ROOT%/.config/jtcc
+call %JMSD_BIN_ROOT%/.config/set_working_directory
 
 set JMSD_BIN_ROOT=
-
-d:
-cd devj
-cd jitemania
