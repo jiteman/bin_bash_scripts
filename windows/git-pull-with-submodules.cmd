46631bd91147b1c2e7fc5e7c4d74e1f1e43c7d46
jiteman <jiteman@gmail.com>
2018-12-25 19:26:36 +0000

    git pull continue its multiplying

--- windows/git-pull-with-submodules.cmd
+++ windows/git-pull-with-submodules.cmd
@@ -1,9 +1,4 @@
 @echo off
 
-FOR /D %%F IN ( "*.*" ) DO (
-	echo [git pull with submodules]: %%~nxF
-	cd %%~nxF
-	git pull origin master
-	git submodule update --recursive --remote
-	cd ..
-)
+git pull origin master
+git submodule update --recursive --remote
