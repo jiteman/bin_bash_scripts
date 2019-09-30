ab665324ca03b095db04c9b61c2014707c9a6c94
jiteman <jiteman@gmail.com>
2019-09-30 14:59:13 +0300

    (windows) boost library support has been added

--- windows/jinit.cmd
+++ windows/jinit.cmd
@@ -8,6 +8,7 @@ call %JMSD_BIN_ROOT%/.config/jvc
 call %JMSD_BIN_ROOT%/.config/jqt
 call %JMSD_BIN_ROOT%/.config/jjom
 call %JMSD_BIN_ROOT%/.config/jtcc
+call %JMSD_BIN_ROOT%/.config/jboost
 call %JMSD_BIN_ROOT%/.config/set_starting_directory
 
 set JMSD_BIN_ROOT=
