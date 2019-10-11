a49e2349fbbbf24a44ad464d59583e28a1b9efdb
jiteman <jiteman@gmail.com>
2019-10-11 11:42:01 +0300

    date and time in 7z add script has been updated

--- windows/7zadddt.cmd
+++ windows/7zadddt.cmd
@@ -1,6 +1,8 @@
 @echo off
-For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
-For /f "tokens=1-2 delims=/:" %%a in ("%TIME: =0%") do (set mytime=%%a%%b)
+for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
+set today=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2%-%ldt:~8,2%%ldt:~10,2%
+echo Local date and time is [%today%]
+
 set today=-%mydate%-%mytime%
 echo "today is %today%"
 7z a -t7z -m0=lzma2 -mx=9 -mmt=on -ms=on "%1%today%.7z" %1 -xr!.svn
