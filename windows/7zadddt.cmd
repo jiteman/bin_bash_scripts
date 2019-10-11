5ee9de55add60419fa00a8accedaac64e997a608
jiteman <jiteman@gmail.com>
2019-10-11 13:35:43 +0300

    windows 7z script date-time has been fixed

--- windows/7zadddt.cmd
+++ windows/7zadddt.cmd
@@ -1,8 +1,7 @@
 @echo off
 for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
-set today=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2%-%ldt:~8,2%%ldt:~10,2%
+set today=-%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2%-%ldt:~8,2%%ldt:~10,2%
 echo Local date and time is [%today%]
 
-set today=-%mydate%-%mytime%
 echo "today is %today%"
 7z a -t7z -m0=lzma2 -mx=9 -mmt=on -ms=on "%1%today%.7z" %1 -xr!.svn
