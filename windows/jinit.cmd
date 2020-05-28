91706f63542ef33d9cef73b4c81bd3684cb4d655
jiteman <jiteman@gmail.com>
2020-05-28 12:30:42 +0000

    vs version init has been separated

--- windows/jinit.cmd
+++ windows/jinit.cmd
@@ -4,7 +4,6 @@ rem @echo on
 
 set JMSD_BIN_ROOT=%~dp0
 
-call %JMSD_BIN_ROOT%/.config/jvc
 call %JMSD_BIN_ROOT%/.config/jqt
 call %JMSD_BIN_ROOT%/.config/jjom
 call %JMSD_BIN_ROOT%/.config/jtcc
---
+++ windows/jinit_bt19.cmd
@@ -0,0 +1,9 @@
+@echo off
+
+rem @echo on
+
+set JMSD_BIN_ROOT=%~dp0
+
+call %JMSD_BIN_ROOT%/.config/jvc_2019_bt
+
+set JMSD_BIN_ROOT=
