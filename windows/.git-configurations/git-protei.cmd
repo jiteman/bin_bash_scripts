9f02f4ad7b6360b89b4271d173def11240c428e6
jiteman <jiteman.ru@ya.ru>
2022-04-18 18:05:44 +0300

    ssh key and credetials have been updated

--- windows/.git-configurations/git-protei.cmd
+++ windows/.git-configurations/git-protei.cmd
@@ -1,3 +1,3 @@
 @rem @cp -T %HOME%\bin\.git-configurations\configurations\.gitconfig.zodiac %HOME%\.gitconfig
-@cp -T C:\bin\.git-configurations\configurations\.gitconfig.zodiac %HOME%\.gitconfig
+@cp -T C:\bin\.git-configurations\configurations\.gitconfig.protei %HOME%\.gitconfig
 @git-user-who
